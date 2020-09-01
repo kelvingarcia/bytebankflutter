b5cf5a2012295af6a86aeac1ac2c85551493f1a0
kelvingarcia <kelvinrgarcia98@gmail.com>
2020-09-01 12:41:10 -0300

    commit inicial

---
+++ android/app/src/main/kotlin/br/com/alura/bytebankflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.alura.bytebankflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+import 'components/transaction_auth_dialog.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
